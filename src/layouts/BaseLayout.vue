e3184e939812fe953ef5e25509ce020844e0620c
anakon <nazarkinah73@gmail.com>
2024-08-25 16:30:32 +0300

    feat(BaseLayout): impl

---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import NavigationMenu from '@/components/NavigationMenu.vue'
+import TopMenu from '@/components/TopMenu.vue'
+</script>
+
+<template>
+  <div class="min-h-screen bg-slate-100">
+    <div class="sticky top-0">
+      <TopMenu />
+      <NavigationMenu />
+    </div>
+    <div class="mx-auto max-w-[1440px] px-[150px] pt-36">
+      <slot />
+    </div>
+  </div>
+</template>
