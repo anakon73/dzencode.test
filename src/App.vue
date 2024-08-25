64dc7f4f5f7998f6490de3cf7e9c3fc34499dd35
anakon <nazarkinah73@gmail.com>
2024-08-25 16:32:44 +0300

    feat(App): impl layout & router view

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
+import BaseLayout from './layouts/BaseLayout.vue'
 </script>
 
 <template>
-  <RouterView />
+  <BaseLayout>
+    <RouterView v-slot="{ Component, route }">
+      <Transition name="fade">
+        <component :is="Component" :key="route.path" />
+      </Transition>
+    </RouterView>
+  </BaseLayout>
 </template>
