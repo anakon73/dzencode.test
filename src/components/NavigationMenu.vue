2c69182ae56d3373d00fded6fe3cb19bc209f02f
anakon <nazarkinah73@gmail.com>
2024-08-25 16:29:48 +0300

    feat(NavigationMenu): impl

---
+++ src/components/NavigationMenu.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { Settings, User } from 'lucide-vue-next'
+import { cn } from '@/utils'
+</script>
+
+<template>
+  <div
+    class="
+      absolute top-0 flex min-h-screen w-36 flex-col items-center justify-center gap-20
+      bg-white px-8 shadow-md
+    "
+  >
+    <div class="relative">
+      <User class="size-20 rounded-full bg-[#6CA533] text-white" />
+      <Settings
+        class="absolute -bottom-2 -right-2 size-8 rounded-full bg-white p-2 shadow-lg"
+      />
+    </div>
+    <div class="flex flex-col items-center gap-3 text-lg font-semibold text-gray-500">
+      <RouterLink
+        v-slot="{ isActive }"
+        :to="{ name: 'Products' }"
+      >
+        <p :class="cn(isActive && 'font-bold underline')">
+          {{ $t('navbar.products') }}
+        </p>
+      </RouterLink>
+      <RouterLink
+        v-slot="{ isActive }"
+        :to="{ name: 'Orders' }"
+      >
+        <p :class="cn(isActive && 'font-bold underline')">
+          {{ $t('navbar.orders') }}
+        </p>
+      </RouterLink>
+      <p>{{ $t('navbar.groups') }}</p>
+      <p>{{ $t('navbar.users') }}</p>
+      <p>{{ $t('navbar.settings') }}</p>
+    </div>
+  </div>
+</template>
