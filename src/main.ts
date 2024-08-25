43840af29da16561424e2cc7fabc5c83d2836366
anakon <nazarkinah73@gmail.com>
2024-08-25 16:33:01 +0300

    feat(store): remove

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,11 @@ import { i18n } from './i18n'
 import { router } from './router'
 import App from './App.vue'
 
-const pinia = createPinia()
 const app = createApp(App)
 
 worker.start({ onUnhandledRequest: 'bypass' })
 
 app.use(VueQueryPlugin)
-app.use(pinia)
 app.use(i18n)
 app.use(router)
 app.mount('#app')
