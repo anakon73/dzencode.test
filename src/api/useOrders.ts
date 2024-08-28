703bd549a6e69bbc9b6b2930f45b5bd2af704a80
anakon <nazarkinah73@gmail.com>
2024-08-28 19:03:34 +0300

    feat(api): delete order composable

--- src/api/useOrders.ts
+++ src/api/useOrders.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/vue-query'
+import { useMutation, useQuery } from '@tanstack/vue-query'
 import type { Order } from '@/types'
 
 export function useOrders() {
@@ -8,6 +8,17 @@ export function useOrders() {
       const r = await fetch('/orders')
       if (!r.ok)
         throw new Error('Not found orders')
+      return r.json()
+    },
+  })
+}
+
+export function useOrderDelete() {
+  return useMutation({
+    mutationFn: async (id: number) => {
+      const r = await fetch(`/orders/${id}`, { method: 'DELETE' })
+      if (!r.ok)
+        throw new Error('Order not found')
       return r.json()
     },
   })
