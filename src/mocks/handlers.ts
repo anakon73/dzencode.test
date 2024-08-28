fdaf998efea6f943a4cda55d53adcda09cc8a273
anakon <nazarkinah73@gmail.com>
2024-08-28 19:03:53 +0300

    feat(mocks): delete order handler & save orders in localStorage

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,12 +1,37 @@
 import { HttpResponse, http } from 'msw'
 import products from '@/api/products.db'
 import orders from '@/api/orders.db'
+import type { Order } from '@/types'
+
+function loadOrders(): Order[] {
+  const storedOrders = localStorage.getItem('orders')
+  return storedOrders ? JSON.parse(storedOrders) : orders
+}
+
+function saveOrders(ordersToSave: Order[]) {
+  localStorage.setItem('orders', JSON.stringify(ordersToSave))
+}
 
 export const handlers = [
   http.get('/products', () => {
     return HttpResponse.json(products)
   }),
   http.get('/orders', () => {
-    return HttpResponse.json(orders)
+    const currentOrders = loadOrders()
+    return HttpResponse.json(currentOrders)
+  }),
+  http.delete('/orders/:id', ({ params }) => {
+    const { id } = params
+    const currentOrders = loadOrders()
+
+    const index = currentOrders.findIndex(order => order.id === +id)
+    if (index !== -1) {
+      currentOrders.splice(index, 1)
+      saveOrders(currentOrders)
+      return HttpResponse.json(currentOrders, { status: 200 })
+    }
+    else {
+      return HttpResponse.json({ message: 'Order not found' }, { status: 404 })
+    }
   }),
 ]
