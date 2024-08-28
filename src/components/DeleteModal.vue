4af255c18eee68155da9433f9185b3c674f7279a
anakon <nazarkinah73@gmail.com>
2024-08-28 19:04:11 +0300

    feat(DeleteModal): impl deleting orders

--- src/components/DeleteModal.vue
+++ src/components/DeleteModal.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import { toRefs } from 'vue'
 import { Trash2, X } from 'lucide-vue-next'
+import { useQueryClient } from '@tanstack/vue-query'
 import {
   AlertDialogCancel,
   AlertDialogContent,
@@ -10,15 +12,31 @@ import {
 } from 'radix-vue'
 import type { Order } from '@/types'
 import { t } from '@/i18n'
+import { useOrderDelete } from '@/api/useOrders'
 
 interface Props {
   open: boolean
   order: Order
 }
 
-defineProps<Props>()
+const props = defineProps<Props>()
 
-defineEmits<{ close: [], delete: [] }>()
+const emits = defineEmits<{ close: [] }>()
+
+const { order } = toRefs(props)
+
+const queryClient = useQueryClient()
+
+const { mutate } = useOrderDelete()
+
+function removeOrder() {
+  mutate(order.value.id, {
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['orders'] })
+      emits('close')
+    },
+  })
+}
 </script>
 
 <template>
@@ -26,7 +44,7 @@ defineEmits<{ close: [], delete: [] }>()
     <AlertDialogPortal>
       <AlertDialogOverlay
         class="fixed inset-0 z-30 bg-black/25"
-        @click.stop="$emit('close')"
+        @click.stop="emits('close')"
       />
       <AlertDialogContent
         class="
@@ -41,7 +59,7 @@ defineEmits<{ close: [], delete: [] }>()
       >
         <AlertDialogCancel
           class="absolute -right-3 -top-3 rounded-full border bg-white p-2"
-          @click.stop="$emit('close')"
+          @click.stop="emits('close')"
         >
           <X class="size-5 text-gray-500" />
         </AlertDialogCancel>
@@ -70,11 +88,12 @@ defineEmits<{ close: [], delete: [] }>()
             text-white
           "
         >
-          <button @click.stop="$emit('close')">
+          <button @click.stop="emits('close')">
             {{ t('order.modalCancel') }}
           </button>
           <button
             class="flex items-center gap-2 rounded-[20px] bg-white px-8 py-2 text-red-600"
+            @click="removeOrder"
           >
             <Trash2 class="size-4" />
             <p>{{ t('order.modalConfirm') }}</p>
